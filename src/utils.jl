a7cbab391d307acb31b1635d5ef0ae5cf5af0ee8
Maximilian Hübl <maximilian.huebl@ist.ac.at>
2024-09-24 10:34:48 +0200

    path iterator

--- src/utils.jl
+++ src/utils.jl
@@ -188,3 +188,85 @@ function find_nth(f::Function, A, n)
     return nothing, j
 end
 find_nth(A, n) = find_nth(identity, A, n)
+
+
+mutable struct PathIterator{G,T}
+    g::G
+    v::T
+    w::T
+end
+PathIterator(g, v, w) = PathIterator(g, promote(v, w)...)
+Base.IteratorSize(::PathIterator) = Base.SizeUnknown()
+
+function Base.iterate(pathitr::PathIterator)
+    path = zeros(Int, nv(pathitr.g))
+    path[pathitr.v] = 1
+    path = complete_path!(path, pathitr.g, pathitr.w)    
+    return copy(path), path #TODO: optimize this
+end
+
+function Base.iterate(pathitr::PathIterator, state)
+    path = state
+    neighs = zeros(Int, nv(pathitr.g))
+
+    path_length = maximum(path)
+
+    u = pathitr.w
+    for _ in 1:path_length-1
+        k = path[u]
+        path[u] = 0
+
+        parent = findfirst(x->x==k-1, path)
+        n_neighs = NautyGraphs.outneighbors!(neighs, pathitr.g, parent)
+        idx = @views searchsortedfirst(neighs[1:n_neighs], u)
+
+        if idx < n_neighs && path[neighs[idx + 1]] == 0
+            path[neighs[idx + 1]] = k
+            path = complete_path!(path, pathitr.g, pathitr.w, neighs)
+            return copy(path), path
+        else
+            k -= 1
+            u = parent
+        end
+    end
+
+    return nothing
+end
+
+function complete_path!(path, g, w, neighs=nothing)
+    ## Completes the path by depth first traversal
+    ## Neighbors are picked in order of vertex number (NOT label)
+    neighs = isnothing(neighs) ? zeros(Int, length(path)) : neighs
+
+    v = argmax(path)
+    v_last = 0
+    k = sum(!iszero, path) + 1
+
+    while path[w] == 0
+        n_neighs = NautyGraphs.outneighbors!(neighs, g, v)
+        n0 = let i=findfirst(x->x==v_last, @view neighs[1:n_neighs]) # use searchsortedfirst
+            isnothing(i) ? 1 : i+1
+        end
+        v_last = 0
+
+        success = false
+        for neigh in @view neighs[n0:n_neighs] # Assume neighbors are sorted
+            if path[neigh] == 0
+                v = neigh
+                path[v] = k
+                k += 1
+                success = true
+                break
+            end
+        end
+
+        if !success
+            path[v] = 0
+            k -= 1
+            v_last = v
+            v = argmax(path)
+        end
+    end
+
+    return path
+end
